4faea8e6be992d898c4792f13643c6821ae0396c
Craig Mills <craigmillsza@yahoo.com>
2021-06-24 16:02:37 +0100

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Flexbox_Activities.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Guitars()
+        {
+         
+            return View();
+        }
+
+        public ActionResult Quick()
+        {
+
+            return View();
+        }
+
+        public ActionResult NewDesign()
+        {
+
+            return View();
+        }
+    }
+}
